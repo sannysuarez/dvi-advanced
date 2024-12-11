d27e8abc311c6326448d0d9fe29f0cd139dd5d2b
sanny suarez <sannysuarez4@gmail.com>
2024-12-11 19:50:50 +0100

    Fixing API

--- dvi/blog.py
+++ dvi/blog.py
@@ -6,9 +6,22 @@ from dvi.utils import  custom_humanize_time
 
 bp = Blueprint('blog', __name__)
 
-@bp.route('/')
+@bp.route('/', methods=['POST', 'GET'])
 def index():
     db = get_db()
+
+    if request.method == 'POST':
+        body = request.form['body']
+
+        if not body:
+            error = 'Empty post you trying to share!'
+            flash(error, 'error')
+            return redirect(url_for('blog.index'))
+
+        db.execute('INSERT INTO post (body, author_id) VALUES (?, ?)', (body, g.user['id']))
+        db.commit()
+        return redirect(url_for('blog.index'))
+
     posts = db.execute(
         'SELECT p.id, body, created, author_id, pic_path, full_name, username FROM post p JOIN user u ON p.author_id = '
         'u.id ORDER BY created DESC'
